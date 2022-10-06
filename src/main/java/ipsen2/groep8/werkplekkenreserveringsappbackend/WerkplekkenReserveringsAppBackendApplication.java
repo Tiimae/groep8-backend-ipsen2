432fc3bc2b0af5881d2efb0d4c28cde9c782f1d1
Tim de kok <de.kok.ac@gmail.com>
2022-10-06 13:57:19 +0200

    initial commit

---
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/WerkplekkenReserveringsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package ipsen2.groep8.werkplekkenreserveringsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerkplekkenReserveringsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerkplekkenReserveringsAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ipsen2/groep8/werkplekkenreserveringsappbackend/WerkplekkenReserveringsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ipsen2.groep8.werkplekkenreserveringsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerkplekkenReserveringsAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
