2de4d1da15f85d4283f8aafc2489637b1ac7ae42
Tim de kok <de.kok.ac@gmail.com>
2022-10-10 17:23:07 +0200

    feat: added functions for get, update and delete in controller and DAO

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/UserDAO.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/UserDAO.java
@@ -4,6 +4,9 @@ import ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.repository.UserReposi
 import ipsen2.groep8.werkplekkenreserveringsappbackend.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class UserDAO {
     private UserRepository userRepository;
@@ -12,7 +15,23 @@ public class UserDAO {
         this.userRepository = userRepository;
     }
 
+    public Optional<User> getUserFromDatabase(String userid) {
+        return this.userRepository.findById(userid);
+    }
+
+    public List<User> getAllUsersFromDatabase() {
+        return this.userRepository.findAll();
+    }
+
     public void saveUserToDatabase(User user) {
         this.userRepository.save(user);
+    }
+
+    public void updateUserInDatabase(User user) {
+        this.userRepository.save(user);
+    }
+
+    public void deleteUserFromDatabase(String userid) {
+        this.userRepository.deleteById(userid);
     }
 }
--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/UserController.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/UserController.java
@@ -3,10 +3,10 @@ package ipsen2.groep8.werkplekkenreserveringsappbackend.controller;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.UserDAO;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/api/user")
@@ -17,12 +17,36 @@ public class UserController {
         this.userDAO = userDAO;
     }
 
+    @RequestMapping(value = "/{userid}", method = RequestMethod.GET)
+    @ResponseBody
+    public Optional<User> getUser(@PathVariable String userid) {
+        return this.userDAO.getUserFromDatabase(userid);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> getUsers() {
+        return this.userDAO.getAllUsersFromDatabase();
+    }
+
     @RequestMapping(value = "", method = RequestMethod.POST, consumes = {"application/json"})
     @ResponseBody
     public String postUser(@RequestBody User user) {
-//        System.out.println(user.getName());
-//        user.setName(user.getName().en));
         this.userDAO.saveUserToDatabase(user);
         return "User has been posted to the database";
+    }
+
+    @PutMapping(value = "")
+    @ResponseBody
+    public String updateUser(@RequestBody User user) {
+        this.userDAO.updateUserInDatabase(user);
+        return "User has been updated";
+    }
+
+    @DeleteMapping(value = "/{userid}")
+    @ResponseBody
+    public String deleteUser(@PathVariable String userid) {
+        this.userDAO.deleteUserFromDatabase(userid);
+        return "User has been deleted";
     }
 }
