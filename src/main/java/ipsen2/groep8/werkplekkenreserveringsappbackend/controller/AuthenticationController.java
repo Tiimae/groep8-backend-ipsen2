ae558f7439325970fb4cfde60c7fcaa8677bd830
da-tokkies <timblommesteijn@gmail.com>
2022-10-13 12:53:51 +0200

    #JWT-Authentication - added target to gitignore - added login and register routes - added classes for creating jwt - added middleware - added service for userDetails - added packages for authentication and security - addded start-class in pom.xml - SecurityConfig for api calls - added findByEmail in userRepository

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/AuthenticationController.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/AuthenticationController.java
@@ -1,37 +1,59 @@
 package ipsen2.groep8.werkplekkenreserveringsappbackend.controller;
 
 import ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.UserDAO;
+import ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.repository.UserRepository;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.model.User;
+import ipsen2.groep8.werkplekkenreserveringsappbackend.security.JWTUtil;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.naming.AuthenticationException;
+import java.util.Collections;
+import java.util.Map;
+
 @Controller
 @RequestMapping(value = "/api/auth")
 public class AuthenticationController {
 
-    private final AuthenticationService authenticationService;
-    private final UserDAO userDAO;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    public AuthenticationController(UserDAO userDAO) {
-        this.authenticationService = new AuthenticationService(AuthenticationService.DEFAULT_COST);
-        this.userDAO = userDAO;
-    }
 
     @PostMapping(value = "/register", consumes = {"application/json"})
     @ResponseBody
-    public String register(@RequestBody User user) {
-        this.userDAO.saveUserToDatabase(user);
-        return "a";
+    public Map<String, Object> register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user = userRepo.save(user);
+        String token = jwtUtil.generateToken(user.getEmail());
+
+        return Collections.singletonMap("jwt-token", token);
     }
 
     @PostMapping(value = "/login", consumes = {"application/json"})
     @ResponseBody
-    public String login(@RequestBody User user) {
-//        this.buildingDAO.updateBuildingInDatabase(building);
-        return "User has been updated";
+    public Map<String, Object> login(@RequestBody User user) throws AuthenticationException {
+
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+
+        authManager.authenticate(authInputToken);
+
+        String token = jwtUtil.generateToken(user.getEmail());
+
+        return Collections.singletonMap("jwt-token", token);
     }
 }
