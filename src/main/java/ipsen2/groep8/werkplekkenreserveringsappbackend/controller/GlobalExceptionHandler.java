b2fce89592b9dd78cb7bf8180a22b740c25ef7f4
wouter <wouter@debruijn.dev>
2022-10-15 18:08:29 +0200

    feat(validation): added EntryNotFoundException and handler

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/GlobalExceptionHandler.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ipsen2.groep8.werkplekkenreserveringsappbackend.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import ipsen2.groep8.werkplekkenreserveringsappbackend.exceptions.EntryNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,6 +13,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntryNotFoundException.class)
+    protected ResponseEntity<Object> handleEntryNotFound(EntryNotFoundException ex) {
+        Map<String, String> res = new HashMap<>();
+        res.put("error", ex.getMessage());
+
+        return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
---
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/exceptions/EntryNotFoundException.java
@@ -0,0 +1,7 @@
+package ipsen2.groep8.werkplekkenreserveringsappbackend.exceptions;
+
+public class EntryNotFoundException extends Exception {
+    public EntryNotFoundException(String message) {
+        super(message);
+    }
+}
