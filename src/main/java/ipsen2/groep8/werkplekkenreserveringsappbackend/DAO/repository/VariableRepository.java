dbf0d441371b685a1f005ea2e871c60fb32b1509
Tim de Kok <de.kok.ac@gmail.com>
2022-11-02 21:14:29 +0100

    feat: removed all variables

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/repository/VariableRepository.java
+++
@@ -1,8 +0,0 @@
-package ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.repository;
-
-import ipsen2.groep8.werkplekkenreserveringsappbackend.model.Variable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VariableRepository extends JpaRepository<Variable, String> {
-}
-
