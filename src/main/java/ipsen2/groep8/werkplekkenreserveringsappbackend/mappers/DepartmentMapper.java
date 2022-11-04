241ff6c00d5ef3aadd15b9cc5dce68ef9ba04214
Tim de Kok <de.kok.ac@gmail.com>
2022-11-04 18:58:49 +0100

    feat: added java doc comments for buildingMapper and DepartmentMapper

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/mappers/DepartmentMapper.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/mappers/DepartmentMapper.java
@@ -16,14 +16,36 @@ import java.util.stream.Collectors;
 
 @Component
 public class DepartmentMapper {
+
+    /**
+     * This is the variable for the WingDAO in the class
+     */
     private WingDAO wingDAO;
+
+    /**
+     * This is the variable for the UserDAO in the class
+     */
     private UserDAO userDAO;
 
+    /**
+     * This is the consturctor of the DepartmentMapper. It set the WingDAO and the UserDAO
+     *
+     * @param wingDAO The DAO for wing
+     * @param userDAO The DAO for user
+     * @author Tim de Kok
+     */
     public DepartmentMapper(WingDAO wingDAO, UserDAO userDAO) {
         this.wingDAO = wingDAO;
         this.userDAO = userDAO;
     }
 
+    /**
+     * This function will be used to create a new department in the database
+     *
+     * @param departmentDTO The department data to create a new department
+     * @return a new department
+     * @author Tim de Kok
+     */
     public Department toDepartment(DepartmentDTO departmentDTO) {
         String name = departmentDTO.getName();
 
@@ -40,6 +62,14 @@ public class DepartmentMapper {
         return new Department(name, users, wings);
     }
 
+    /**
+     * This function returns an update department, so we can save it in the database
+     *
+     * @param base The department data that already exist in the database
+     * @param update The department data to create a new department
+     * @return an updated department
+     * @author Tim de Kok
+     */
     public Department updateDepartment(Department base, Department update) {
         base.setName(update.getName());
         base.setUsers(update.getUsers());
