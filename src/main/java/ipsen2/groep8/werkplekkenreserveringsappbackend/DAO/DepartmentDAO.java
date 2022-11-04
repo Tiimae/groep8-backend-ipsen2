ac700714d58a1a9327c3bb3bb99c00ff4378011c
Tim de Kok <de.kok.ac@gmail.com>
2022-11-04 19:52:11 +0100

    feat: added java doc comments for BuildingDAO, DepartmentDAO and MeetingRoomDAO

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/DepartmentDAO.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/DepartmentDAO.java
@@ -16,32 +16,78 @@ import java.util.Set;
 @Component
 public class DepartmentDAO {
 
+    /**
+     * This is the variable for the DepartmentRepository in the class
+     */
     private DepartmentRepository departmentRepository;
+
+    /**
+     * This is the variable for the DepartmentMapper in the class
+     */
     private DepartmentMapper departmentMapper;
 
+    /**
+     * This is the constructor of the departmentDAO. It set the departmentMapper and departmentRepository
+     *
+     * @param departmentRepository The repository for department
+     * @param departmentMapper The Mapper for department
+     * @author Tim de Kok
+     */
     public DepartmentDAO(DepartmentRepository departmentRepository, DepartmentMapper departmentMapper) {
         this.departmentRepository = departmentRepository;
         this.departmentMapper = departmentMapper;
     }
 
+    /**
+     * Get a specific department out of the database by id and returns that department
+     *
+     * @param departmentid The id of the department you want to have
+     * @return A department out of the database
+     * @author Tim de Kok
+     */
     public Optional<Department> getDepartmentFromDatabase(String departmentid) {
         return this.departmentRepository.findById(departmentid);
     }
 
+    /**
+     * Get all departments out of the database and returns it as a list of departments
+     *
+     * @return A list of departments out of the database
+     * @author Tim de Kok
+     */
     public List<Department> getAllDepartmentsFromDatabase() {
         return this.departmentRepository.findAll();
     }
 
+    /**
+     * Save a new department to the database
+     *
+     * @param department The department that needs to be safed in the database
+     * @author Tim de Kok
+     */
     public void postDepartmentToDatabase(Department department) {
         this.departmentRepository.save(department);
     }
 
+    /**
+     * Update a existing department in the database
+     *
+     * @param id The id of the department what needs to be update
+     * @param departmentUpdate The updated version of the department
+     * @author Tim de Kok
+     */
     public void updateDepartmentInDatabase(String id, Department departmentUpdate) {
         Department department = this.departmentRepository.getById(id);
         department = this.departmentMapper.updateDepartment(department, departmentUpdate);
         this.departmentRepository.saveAndFlush(department);
     }
 
+    /**
+     * Removes a department out of the database
+     *
+     * @param departmentId Id of the department what needs to be removed
+     * @author Tim de Kok
+     */
     public void removeDepartmentFromDatabase(String departmentId) {
         final Department department = this.departmentRepository.findById(departmentId).get();
         for (User user : department.getUsers()) {
--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/MeetingRoomDAO.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/DAO/MeetingRoomDAO.java
@@ -9,28 +9,68 @@ import java.util.Optional;
 
 @Component
 public class MeetingRoomDAO {
+    /**
+     * This is the variable for the MeetingRoomRepository in the class
+     */
     private MeetingRoomRepository meetingRoomRepository;
 
+    /**
+     * This is the constructor of the MeetingRoomDAO. It set the meetingRoomRepository
+     *
+     * @param meetingRoomRepository The repository for meeting room
+     * @author Tim de Kok
+     */
     public MeetingRoomDAO(MeetingRoomRepository meetingRoomRepository) {
         this.meetingRoomRepository = meetingRoomRepository;
     }
 
+    /**
+     * Get a specific meeting room out of the database by id and returns that meeting room
+     *
+     * @param meetingRoomId The id of the meeting room you want to have
+     * @return A meeting room out of the database
+     * @author Tim de Kok
+     */
     public Optional<MeetingRoom> getMeetingRoomFromDatabase(String meetingRoomId) {
         return this.meetingRoomRepository.findById(meetingRoomId);
     }
 
+    /**
+     * Get all meeting rooms out of the database and returns it as a list of meeting rooms
+     *
+     * @return A list of meeting rooms out of the database
+     * @author Tim de Kok
+     */
     public List<MeetingRoom> getAllMeetingRoomsFromDatabase() {
         return this.meetingRoomRepository.findAll();
     }
 
+    /**
+     * Save a new meeting room to the database
+     *
+     * @param meetingRoom The meeting room what needs to be safed in the database
+     * @author Tim de Kok
+     */
     public void saveMeetingRoomToDatabase(MeetingRoom meetingRoom) {
         this.meetingRoomRepository.save(meetingRoom);
     }
 
+    /**
+     * Update a existing meeting room in the database
+     *
+     * @param meetingRoom The updated version of the meeting room
+     * @author Tim de Kok
+     */
     public void updateMeetingRoomInDatabase(MeetingRoom meetingRoom) {
         this.meetingRoomRepository.saveAndFlush(meetingRoom);
     }
 
+    /**
+     * Removes a meeting room out of the database
+     *
+     * @param meetingRoomId Id of the meeting room what needs to be removed
+     * @author Tim de Kok
+     */
     public void deleteMeetingRoomFromDatabase(String meetingRoomId) {
         final MeetingRoom meetingRoom = this.meetingRoomRepository.getById(meetingRoomId);
 
