0cecdcecbf12c93661d171b739c92221076634f6
Frederik Coster <costerfrederik@gmail.com>
2022-11-07 03:03:23 +0100

    Style: Email styling
    added better styling for email

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/service/EmailService.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/service/EmailService.java
@@ -18,7 +18,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(mail, true);
 
         // rewrite this with a template
-        String text = "<html><head></head><body style=\"display: flex; flex-direction: column\"><img src=\"https://cstories.nl/wp-content/uploads/2018/02/Cgi-logo-svg-420x261.png\" style=\"display: block; width: 210px; margin-left: auto; margin-right: auto; height: 130px; \"><div style=\"display: flex; flex-direction: column; width: 100%; \"><h1>"+subject+"</h1><hr style=\"border: 1px block solid\"><p>"+body +"</p></div></body></html>";
+        String text = "<html><head></head><body style=\"display: flex; flex-direction: column; padding: 30px;\"><img src=\"https://cstories.nl/wp-content/uploads/2018/02/Cgi-logo-svg-420x261.png\" style=\"height: 50px; margin: 0 auto; \"><div style=\"display: flex; flex-direction: column; width: 100%; font-family: sans-serif; text-align: center; \"><h1 style=\"margin: 50px 0 20px 0;\">"+subject+"</h1><hr style=\"border: 1px block solid\"><div style=\"margin: 0;\">"+body +"</div></div></body></html>";
 
         helper.setFrom("email.service.ipsen2@gmail.com");
         helper.setTo(to);
