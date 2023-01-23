185ce24ae2569ae88f602bd6dacb5a0145da1716
Tim de Kok <de.kok.ac@gmail.com>
2023-01-24 00:42:18 +0100

    feat: added mail threading

--- src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/ReservationController.java
+++ src/main/java/ipsen2/groep8/werkplekkenreserveringsappbackend/controller/ReservationController.java
@@ -1,5 +1,29 @@
 package ipsen2.groep8.werkplekkenreserveringsappbackend.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import ipsen2.groep8.werkplekkenreserveringsappbackend.DAO.ReservationDAO;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.DTO.ReservationDTO;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.constant.ApiConstant;
@@ -9,15 +33,7 @@ import ipsen2.groep8.werkplekkenreserveringsappbackend.model.Reservation;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.model.User;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.service.ApiResponseService;
 import ipsen2.groep8.werkplekkenreserveringsappbackend.service.EmailService;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.mail.MessagingException;
-import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import ipsen2.groep8.werkplekkenreserveringsappbackend.thread.MailThread;
 
 @RestController
 @RequestMapping(
@@ -40,13 +56,12 @@ public class ReservationController {
     public ApiResponseService<Optional<Reservation>> getReservation(@PathVariable String reservationId) {
         Optional<Reservation> reservation = this.reservationDAO.getReservationFromDatabase(reservationId);
 
-        if (reservation.isEmpty()){
+        if (reservation.isEmpty()) {
             return new ApiResponseService(HttpStatus.NOT_FOUND, "Reservation not found!");
         }
 
         return new ApiResponseService(HttpStatus.FOUND, reservation);
     }
-
 
 
     @GetMapping(value = ApiConstant.getAllReservations)
@@ -56,18 +71,18 @@ public class ReservationController {
         Set<Reservation> filteredReservations = new HashSet<>();
 
 
-        if(filter != null && filter.equals("thismonth")){
+        if (filter != null && filter.equals("thismonth")) {
             for (Reservation reservation : allReservations) {
-                if(LocalDateTime.now().getMonth() == reservation.getStartDate().getMonth()){
+                if (LocalDateTime.now().getMonth() == reservation.getStartDate().getMonth()) {
                     filteredReservations.add(reservation);
                 }
             }
             return new ApiResponseService(HttpStatus.OK, filteredReservations);
         }
 
-        if(filter != null && filter.equals("lastmonth")){
+        if (filter != null && filter.equals("lastmonth")) {
             for (Reservation reservation : allReservations) {
-                if(LocalDateTime.now().minusMonths(1).getMonth() == reservation.getStartDate().getMonth()){
+                if (LocalDateTime.now().minusMonths(1).getMonth() == reservation.getStartDate().getMonth()) {
                     filteredReservations.add(reservation);
                 }
             }
@@ -81,32 +96,29 @@ public class ReservationController {
     public ApiResponseService<Reservation> postReservation(@RequestBody @Valid ReservationDTO reservationDTO) throws EntryNotFoundException {
         Reservation reservation = reservationMapper.toReservation(reservationDTO);
         this.reservationDAO.saveReservationToDatabase(reservation);
+        List<String> meetingRooms = new ArrayList<>();
+        reservation.getMeetingRooms().stream().forEach(meetingRoom -> {
+            meetingRooms.add(meetingRoom.getId());
+        });
 
-        try {
-             List<String> meetingRooms = new ArrayList<>();
-             reservation.getMeetingRooms().stream().forEach(meetingRoom -> {
-                meetingRooms.add(meetingRoom.getId());
-            });
-
-            this.emailService.sendMessage(
+        MailThread mail = new MailThread(
                 reservation.getUser().getEmail(),
                 "Your CGI reservation",
-                "<p>Hi, we have received your reservation for a " + reservation.getType() + ", and confirm your booking.</p><p style=\"margin: 0;\">" + reservation.getStartDate().format(DateTimeFormatter.ofPattern("d MMMM y")) + "</p>" + reservation.getStartDate().toLocalTime() + " - " + reservation.getEndDate().toLocalTime()
-            );
-        } catch (MessagingException e) {
-            System.out.println(e.getMessage());
-        }
+                "<p>Hi, we have received your reservation for a " + reservation.getType() + ", and confirm your booking.</p><p style=\"margin: 0;\">" + reservation.getStartDate().format(DateTimeFormatter.ofPattern("d MMMM y")) + "</p>" + reservation.getStartDate().toLocalTime() + " - " + reservation.getEndDate().toLocalTime(),
+                this.emailService
+        );
+        mail.start();
 
         return new ApiResponseService<>(HttpStatus.CREATED, reservation);
     }
 
-    @PutMapping(value =ApiConstant.getReservation, consumes = {"application/json"})
+    @PutMapping(value = ApiConstant.getReservation, consumes = {"application/json"})
     @ResponseBody
     public ApiResponseService updateReservation(@PathVariable String reservationId, @RequestBody @Valid ReservationDTO reservationDTO) throws EntryNotFoundException {
         return new ApiResponseService(HttpStatus.ACCEPTED, this.reservationDAO.updateReservationInDatabase(reservationId, reservationDTO));
     }
 
-    @PatchMapping(value =ApiConstant.getReservation, consumes = {"application/json"})
+    @PatchMapping(value = ApiConstant.getReservation, consumes = {"application/json"})
     @ResponseBody
     public ApiResponseService patchReservation(@PathVariable String reservationId, @RequestBody @Valid ReservationDTO reservationDTO) throws EntryNotFoundException {
         return new ApiResponseService(HttpStatus.ACCEPTED, this.reservationDAO.updateReservationInDatabase(reservationId, reservationDTO));
